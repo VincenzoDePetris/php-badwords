407a0b09c7cb13a8ee3e233575534c8348773e0c
VincenzoDePetris <vince02@live.it>
2023-09-25 16:20:41 +0200

    first commit

---
+++ badwords.php
@@ -0,0 +1,20 @@
+<?php
+
+$text = $_GET["text"];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>badwords</title>
+</head>
+<body>
+  <p>
+    <?php echo $text ?>
+</p>
+</body>
+</html>
